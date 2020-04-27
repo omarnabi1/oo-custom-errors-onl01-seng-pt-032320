06111637472fb5f9ae5805a4092900103fd79bf6
Omar Nabi <onabi20@gmail.com>
2020-04-27 04:19:21 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -1,25 +1,26 @@
 class Person
   attr_accessor :partner, :name
-
+ 
   def initialize(name)
     @name = name
   end
-
+ 
   def get_married(person)
     self.partner = person
-    person.partner = self
+    if person.class != Person
+      raise PartnerError
+    else
+      person.partner = self
+    end
   end
-  
-class PartnerError < StandardError
+ 
+  class PartnerError < StandardError
   end
 end
-
-end
-
+ 
 beyonce = Person.new("Beyonce")
 beyonce.get_married("Jay-Z")
 puts beyonce.name
-
 
 
 
